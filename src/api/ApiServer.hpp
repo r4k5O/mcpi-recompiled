08b0c3f9930fffdaac4afb9bab2427748be34ac3
r4k5O <oskar.bergatt@gmx.net>
2026-08-19 15:28:30 +0200

    feat: add API server interface

---
+++ src/api/ApiServer.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "api/Command.hpp"
+
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <string>
+
+namespace mcpi::api {
+
+class ApiServer {
+public:
+    using Handler = std::function<std::optional<std::string>(const Command&)>;
+
+    explicit ApiServer(std::uint16_t port = 4711);
+    ~ApiServer();
+
+    ApiServer(const ApiServer&) = delete;
+    ApiServer& operator=(const ApiServer&) = delete;
+    ApiServer(ApiServer&&) = delete;
+    ApiServer& operator=(ApiServer&&) = delete;
+
+    bool start(Handler handler);
+    void stop();
+
+    [[nodiscard]] bool running() const noexcept;
+    [[nodiscard]] std::uint16_t port() const noexcept;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+} // namespace mcpi::api
