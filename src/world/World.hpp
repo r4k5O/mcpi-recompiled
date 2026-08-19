683e6d0c7cbad28f4f59925ab388c73b39da2db0
r4k5O <oskar.bergatt@gmx.net>
2026-08-19 19:44:53 +0200

    feat: expose world height and chunk traversal

--- src/world/World.hpp
+++ src/world/World.hpp
@@ -3,6 +3,7 @@
 #include "world/Chunk.hpp"
 
 #include <cstddef>
+#include <functional>
 #include <unordered_map>
 
 namespace mcpi::world {
@@ -17,10 +18,14 @@ struct BlockPos {
 
 class World {
 public:
-    [[nodiscard]] BlockState block_at(const BlockPos& position) const;
+    [[nodiscard]] BlockState block_at(const BlockPos& position) const noexcept;
     void set_block(const BlockPos& position, const BlockState& block);
+    [[nodiscard]] int height_at(int x, int z) const noexcept;
 
     [[nodiscard]] std::size_t chunk_count() const noexcept;
+    void clear() noexcept;
+
+    void for_each_chunk(const std::function<void(const Chunk&)>& visitor) const;
 
 private:
     struct ChunkPosHash {
@@ -29,6 +34,8 @@ private:
 
     [[nodiscard]] static ChunkPos chunk_position(const BlockPos& position) noexcept;
     [[nodiscard]] static LocalBlockPos local_position(const BlockPos& position) noexcept;
+    [[nodiscard]] static int local_coordinate(int value) noexcept;
+    [[nodiscard]] static int floor_divide_by_chunk_width(int value) noexcept;
 
     std::unordered_map<ChunkPos, Chunk, ChunkPosHash> chunks_;
 };
