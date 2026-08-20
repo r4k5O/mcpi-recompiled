68f0fc40f8cf80f21dc259b682f933f083cb7669
r4k5O <oskar.bergatt@gmx.net>
2026-08-20 14:03:20 +0200

    feat: expose classified MCPI dispatch results

--- src/api/ApiDispatcher.hpp
+++ src/api/ApiDispatcher.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "api/Command.hpp"
+#include "api/CommandResult.hpp"
 #include "game/GameApi.hpp"
 
 #include <optional>
@@ -12,6 +13,7 @@ class ApiDispatcher {
 public:
     explicit ApiDispatcher(game::GameApi& game);
 
+    [[nodiscard]] CommandResult dispatch_result(const Command& command) const;
     [[nodiscard]] std::optional<std::string> dispatch(const Command& command) const;
 
 private:
