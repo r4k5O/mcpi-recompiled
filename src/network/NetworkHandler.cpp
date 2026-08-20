4c01d0b09058e4e07088096a241f7c152ec36f2c
r4k5O <oskar.bergatt@gmx.net>
2026-08-20 13:52:53 +0200

    feat: add network handler translation unit

---
+++ src/network/NetworkHandler.cpp
@@ -0,0 +1,8 @@
+#include "network/NetworkHandler.hpp"
+
+namespace mcpi::network {
+
+// Intentionally empty translation unit. Concrete client/server handlers will
+// only be added when runtime reachability and packet semantics are evidenced.
+
+} // namespace mcpi::network
